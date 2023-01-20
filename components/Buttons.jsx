e70cbc31ef6b9f4836485018e79ba1ca08a87ac6
Andres-XL <amovilla1103@gmail.com>
2023-01-19 21:09:10 -0500

    WIP

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const SquareButton = ({icon, label, onClick, className=''}) =>
-	<button className={`square-btn ${className}`} onClick={onClick}>
-		<FontAwesomeIcon className={'square-btn__icon'}  icon={icon} />
-		<div className={'square-btn__label'}>{label}</div>
-	</button>
+export const SquareButton = ({ icon, label, onClick, className = "" }) => (
+  <button className={`square-btn ${className}`} onClick={onClick}>
+    <FontAwesomeIcon className={"square-btn__icon"} icon={icon} />
+    <div className={"square-btn__label"}>{label}</div>
+  </button>
+);
 
+export const IconButton = ({ icon, onClick, className = "" }) => (
+  <button className={`icon-btn ${className}`} onClick={onClick}>
+    <FontAwesomeIcon className={"icon-btn__icon"} icon={icon} />
+  </button>
+);
 
-export const IconButton = ({icon, onClick, className=''}) =>
-	<button className={`icon-btn ${className}`} onClick={onClick}>
-		<FontAwesomeIcon className={'icon-btn__icon'} icon={icon} />
-	</button>
-
-export const ThemedButton = ({icon, label, onClick, className=''}) =>
-	<button className={`themed-btn ${className}`} onClick={onClick}>
-		{!!icon && <FontAwesomeIcon className={'themed-btn__icon'} icon={icon} />}
-		{!!label && <div className={'themed-btn__label'}>{label}</div>}
-	</button>
+export const ThemedButton = ({
+  icon,
+  label,
+  onClick,
+  className = "",
+  disabled = false,
+}) => (
+  <button
+    disabled={disabled}
+    className={`themed-btn ${className}`}
+    onClick={onClick}
+  >
+    {!!icon && <FontAwesomeIcon className={"themed-btn__icon"} icon={icon} />}
+    {!!label && <div className={"themed-btn__label"}>{label}</div>}
+  </button>
+);
--- components/tools/Tasks.jsx
+++ components/tools/Tasks.jsx
@@ -1,62 +1,73 @@
-import React from 'react';
-import {MovableCard} from '../Cards';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCheck, faPlus, faTimes} from '@fortawesome/free-solid-svg-icons';
-import {ThemedButton} from '../Buttons';
+import React from "react";
+import { MovableCard } from "../Cards";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheck,
+  faLongArrowAltRight,
+  faPlus,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { ThemedButton } from "../Buttons";
 
-export function Tasks({visible, toggleCard}) {
-	const [tasks, setTasks] = React.useState([])
+export function Tasks({ visible, toggleCard }) {
+  const [tasks, setTasks] = React.useState([]);
 
-	const addTask = newTask => setTasks([...tasks, newTask])
-	const toggleTask = key => {
-		const curTasks = [...tasks]
-		curTasks[key].done = !curTasks[key].done
-		setTasks(curTasks)
-	}
+  const addTask = (newTask) => setTasks([...tasks, newTask]);
+  const toggleTask = (key) => {
+    const curTasks = [...tasks];
+    curTasks[key].done = !curTasks[key].done;
+    setTasks(curTasks);
+  };
 
-	return (
-		<MovableCard visible={visible} className={'tasks'} toggleCard={toggleCard}>
-			{tasks.map( (item,i) => <Task
-				key={i}
-				toggleTask={() => toggleTask(i)}
-				{...item}
-			/>)}
-			<TaskInput addTask={addTask}/>
-		</MovableCard>
-	);
+  return (
+    <MovableCard visible={visible} className={"tasks"} toggleCard={toggleCard}>
+      {tasks.map((item, i) => (
+        <Task key={i} toggleTask={() => toggleTask(i)} {...item} />
+      ))}
+      <TaskInput addTask={addTask} />
+    </MovableCard>
+  );
 }
 
-const Task = ({name, done, toggleTask}) => <div
-	className={`task-item ${['','checked'][+done]}`}
-	onClick={toggleTask}
->
-	<div className={'task-item__check-box'}>
-		{done && <FontAwesomeIcon icon={faCheck} />}
-	</div>
-	<div className={'task-item__label'}>
-		{name}
-	</div>
-</div>
+const Task = ({ name, done, toggleTask }) => (
+  <div className={`task-item ${["", "checked"][+done]}`} onClick={toggleTask}>
+    <div className={"task-item__check-box"}>
+      {done && <FontAwesomeIcon icon={faCheck} />}
+    </div>
+    <div className={"task-item__label"}>{name}</div>
+  </div>
+);
 
-const TaskInput = ({addTask}) => {
-	const [name, setName] = React.useState('')
+const TaskInput = ({ addTask }) => {
+  const [name, setName] = React.useState("");
 
-	const handleAdd = () => {
-		addTask({name, done: false})
-		setName('')
-	}
+  const handleAdd = () => {
+    if (name) {
+      addTask({ name, done: false });
+      setName("");
+    }
+  };
 
-	return <div className={'new-task-item'}>
-		<input
-			className={'new-task-item__input'}
-			value={name}
-			onChange={e => setName(e.target.value)}
-		/>
-		<ThemedButton
-			onClick={handleAdd}
-			className={'new-task-item__button'}
-			icon={faPlus}
-			label={'Add task'}
-		/>
-	</div>
-}
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleAdd();
+    }
+  };
+
+  return (
+    <div className={"new-task-item"}>
+      <input
+        className={"new-task-item__input"}
+        onKeyDown={handleEnter}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <ThemedButton
+        disabled={!name}
+        onClick={handleAdd}
+        className={"new-task-item__button"}
+        icon={faPlus}
+      />
+    </div>
+  );
+};
